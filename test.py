76d62c58c0a5026df40405ec95718d4f8087d7f1
DEEPIKA CHOUDHARY <94464583+Deepikachoudhary1@users.noreply.github.com>
2025-01-22 12:57:28 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,71 @@
+3.
+
+arthimeticoperation=
+print(1+1)
+print(1-2)
+print(1%8)
+print(1*2)
+
+#4.factorial number 
+
+num = int(input("Enter a number to calculate its factorial: "))
+factorial = 1
+# Check if the number is negative 5*4*3*2*1 
+if num < 0:
+    print("Factorial does not exist for negative numbers")
+else:
+    while num > 1:
+        factorial *= num  # Multiply factorial by current number
+        num -= 1  # Decrease num by 1 in each iteration
+    print(f"The factorial is {factorial}")
+
+#5.fibnacci number
+
+n=int(input("enter the number:"))
+#0,1,1,2,3,5,8,13,.......
+a=0
+b=1
+print("fibnoacci sequence:")
+for i in range(n):
+    print(a,end="")
+    a,b=b,a+b
+
+
+#6 fizz buzz
+
+num=int(input("enter the number:"))
+for i in value
+        if i%3==0:
+              
+
+# 7 find all divisors
+
+n=int(input("enter the number:"))
+divisor=[]
+for i in range(1,n+1):
+    if n%i==0:
+        divisor.(n)
+
+   print(divisor is a )
+
+ #10 prefect number
+ 1+2+3=6
+n=int(input("enter the number:"))
+sum=0
+for i in range(1,n):
+    if n%i==0:
+        sum=sum+i
+        if sum==n:
+            print("the number is prefect number")
+else:
+    print("the number is not a prefect number:")
+    
+
+
+
+
+   
+
+
+
+
